279c815bb2739697b09c0be40bdb328b3cb9d9f7
Jiong Ye <jye@iqsolutions.com>
2012-02-28 11:34:00 -0500

    move and rename functions

---
+++ FolderFix/FolderFix.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace FolderFix
+{
+    public partial class Form1 : Form
+    {
+        private List<string> folderSets = new List<string>();
+        private string selectedPath = string.Empty;
+        private string ext = "_files";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            FolderList.Columns.Add("#", 25);
+            FolderList.Columns.Add("Path", FolderList.Width - 200);
+            FolderList.Columns.Add("Move Files", 70);
+            FolderList.Columns.Add("Rename Files", 70);
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.RootFolder = Environment.SpecialFolder.Desktop;
+            folderBrowserDialog1.ShowNewFolderButton = false;
+            DialogResult r = folderBrowserDialog1.ShowDialog();
+
+            if (r == DialogResult.OK)
+            {
+                folderSets.Clear();
+                selectedPath = folderBrowserDialog1.SelectedPath;
+
+                if (Directory.Exists(selectedPath))
+                {
+                    //read folders in path
+                    string[] folders = Directory.GetDirectories(selectedPath);
+                    FolderList.Items.Clear();
+                    List<ListViewItem> itemList = new List<ListViewItem>();
+                    int index = 1;
+
+                    foreach (string f in folders)
+                    {
+                        if (!f.EndsWith(ext))
+                        {
+                            string textFolder = f;
+                            string imgFolder = f + ext;
+
+                            if (Directory.Exists(f) && Directory.Exists(imgFolder))
+                            {
+                                if (!folderSets.Contains(textFolder))
+                                {
+                                    int filesToMove = Directory.GetFiles(textFolder).Count();
+                                    folderSets.Add(textFolder);
+
+                                    ListViewItem item = new ListViewItem(index.ToString());
+                                    item.Name = Path.GetFileName(textFolder);
+                                    item.SubItems.Add(Path.GetFileName(textFolder));
+                                    item.SubItems.Add(filesToMove > 0 ? "Yes (" + filesToMove + ")" : "No");
+                                    item.SubItems.Add("Yes");
+                                    itemList.Add(item);
+
+                                    index++;
+                                }
+                            }
+                        }
+                    }
+                    FolderList.Items.AddRange(itemList.ToArray());
+                }
+            }
+        }
+
+        private void moveFilesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (folderSets.Count > 0)
+            {
+                foreach (string f in folderSets)
+                {
+                    string textFolder = f;
+                    string imgFolder = f + ext;
+
+                    if (Directory.Exists(textFolder) && Directory.Exists(imgFolder))
+                    {
+                        string[] textFiles = Directory.GetFiles(textFolder);
+                        foreach (string filePath in textFiles)
+                        {
+                            string fileName = Path.GetFileName(filePath);
+                            File.Move(textFolder + "\\" + fileName, imgFolder + "\\" + fileName);
+                        }
+
+                        Directory.Delete(textFolder);
+
+                        var updatedFolder = FolderList.Items.Find(Path.GetFileName(textFolder), false);
+                        if (updatedFolder != null)
+                        {
+                            updatedFolder[0].SubItems[2].Text = "Moved";
+                        }
+                    }
+                }
+            }
+        }
+
+        private void renameFilesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (folderSets.Count > 0)
+            {
+                foreach (string f in folderSets)
+                {
+                    string textFolder = f;
+                    string imgFolder = f + ext;
+
+                    if (Directory.Exists(imgFolder))
+                    {
+                        Regex number = new Regex(@"\(\d+\)", RegexOptions.IgnoreCase);
+                        string[] imgFiles = Directory.GetFiles(imgFolder);
+                        string folderName = number.Replace(Path.GetFileName(imgFolder).Replace(ext, ""), "").Trim();
+                        int index = 1;
+
+                        foreach (string filePath in imgFiles)
+                        {
+                            File.Copy(filePath, imgFolder + "\\" + folderName + "-" + (index++).ToString() + Path.GetExtension(filePath));
+                            File.Delete(filePath);
+                        }
+
+                        var updatedFolder = FolderList.Items.Find(Path.GetFileName(textFolder), false);
+                        if (updatedFolder != null)
+                        {
+                            updatedFolder[0].SubItems[3].Text = "Renamed";
+                        }
+                    }
+                }
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+            this.Close();
+        }
+    }
+}
--- FolderFix/Form1.Designer.cs
+++ FolderFix/Form1.Designer.cs
@@ -28,23 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.OpenFolderButton = new System.Windows.Forms.Button();
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.FolderList = new System.Windows.Forms.ListView();
-            this.MoveFilesButton = new System.Windows.Forms.Button();
-            this.RenameFilesButton = new System.Windows.Forms.Button();
-            this.ResultBox = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.filesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.actionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.moveFilesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.renameFilesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // OpenFolderButton
-            // 
-            this.OpenFolderButton.Location = new System.Drawing.Point(13, 13);
-            this.OpenFolderButton.Name = "OpenFolderButton";
-            this.OpenFolderButton.Size = new System.Drawing.Size(266, 23);
-            this.OpenFolderButton.TabIndex = 0;
-            this.OpenFolderButton.Text = "Open Folder";
-            this.OpenFolderButton.UseVisualStyleBackColor = true;
-            this.OpenFolderButton.Click += new System.EventHandler(this.OpenFolderButton_Click);
             // 
             // FolderList
             // 
@@ -52,60 +46,82 @@
                         | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
             this.FolderList.GridLines = true;
-            this.FolderList.Location = new System.Drawing.Point(13, 42);
+            this.FolderList.Location = new System.Drawing.Point(13, 27);
             this.FolderList.Name = "FolderList";
-            this.FolderList.Size = new System.Drawing.Size(927, 434);
+            this.FolderList.Size = new System.Drawing.Size(507, 408);
             this.FolderList.TabIndex = 2;
             this.FolderList.UseCompatibleStateImageBehavior = false;
             this.FolderList.View = System.Windows.Forms.View.Details;
             // 
-            // MoveFilesButton
+            // menuStrip1
             // 
-            this.MoveFilesButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.MoveFilesButton.Location = new System.Drawing.Point(285, 13);
-            this.MoveFilesButton.Name = "MoveFilesButton";
-            this.MoveFilesButton.Size = new System.Drawing.Size(370, 23);
-            this.MoveFilesButton.TabIndex = 3;
-            this.MoveFilesButton.Text = "MoveFiles";
-            this.MoveFilesButton.UseVisualStyleBackColor = true;
-            this.MoveFilesButton.Click += new System.EventHandler(this.MoveFilesButton_Click);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.filesToolStripMenuItem,
+            this.actionsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(532, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
             // 
-            // RenameFilesButton
+            // filesToolStripMenuItem
             // 
-            this.RenameFilesButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.RenameFilesButton.Location = new System.Drawing.Point(661, 13);
-            this.RenameFilesButton.Name = "RenameFilesButton";
-            this.RenameFilesButton.Size = new System.Drawing.Size(279, 23);
-            this.RenameFilesButton.TabIndex = 4;
-            this.RenameFilesButton.Text = "Rename Files";
-            this.RenameFilesButton.UseVisualStyleBackColor = true;
+            this.filesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.filesToolStripMenuItem.Name = "filesToolStripMenuItem";
+            this.filesToolStripMenuItem.Size = new System.Drawing.Size(42, 20);
+            this.filesToolStripMenuItem.Text = "Files";
             // 
-            // ResultBox
+            // openToolStripMenuItem
             // 
-            this.ResultBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.ResultBox.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.ResultBox.ForeColor = System.Drawing.Color.White;
-            this.ResultBox.Location = new System.Drawing.Point(13, 482);
-            this.ResultBox.Multiline = true;
-            this.ResultBox.Name = "ResultBox";
-            this.ResultBox.ReadOnly = true;
-            this.ResultBox.Size = new System.Drawing.Size(927, 42);
-            this.ResultBox.TabIndex = 5;
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // actionsToolStripMenuItem
+            // 
+            this.actionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.moveFilesToolStripMenuItem,
+            this.renameFilesToolStripMenuItem});
+            this.actionsToolStripMenuItem.Name = "actionsToolStripMenuItem";
+            this.actionsToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.actionsToolStripMenuItem.Text = "Actions";
+            // 
+            // moveFilesToolStripMenuItem
+            // 
+            this.moveFilesToolStripMenuItem.Name = "moveFilesToolStripMenuItem";
+            this.moveFilesToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.moveFilesToolStripMenuItem.Text = "Move Files";
+            this.moveFilesToolStripMenuItem.Click += new System.EventHandler(this.moveFilesToolStripMenuItem_Click);
+            // 
+            // renameFilesToolStripMenuItem
+            // 
+            this.renameFilesToolStripMenuItem.Name = "renameFilesToolStripMenuItem";
+            this.renameFilesToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.renameFilesToolStripMenuItem.Text = "Rename Files";
+            this.renameFilesToolStripMenuItem.Click += new System.EventHandler(this.renameFilesToolStripMenuItem_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(952, 536);
-            this.Controls.Add(this.ResultBox);
-            this.Controls.Add(this.RenameFilesButton);
-            this.Controls.Add(this.MoveFilesButton);
+            this.ClientSize = new System.Drawing.Size(532, 447);
             this.Controls.Add(this.FolderList);
-            this.Controls.Add(this.OpenFolderButton);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Folder Fix";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -113,12 +129,15 @@
 
         #endregion
 
-        private System.Windows.Forms.Button OpenFolderButton;
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
         private System.Windows.Forms.ListView FolderList;
-        private System.Windows.Forms.Button MoveFilesButton;
-        private System.Windows.Forms.Button RenameFilesButton;
-        private System.Windows.Forms.TextBox ResultBox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem filesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem actionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem moveFilesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem renameFilesToolStripMenuItem;
     }
 }
 
