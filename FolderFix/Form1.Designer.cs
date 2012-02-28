83e7dbaadbc9380a65d71191f68953f81160095a
Jiong Ye <jye@iqsolutions.com>
2012-02-27 22:44:28 -0500

    first import

---
+++ FolderFix/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace FolderFix
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OpenFolderButton = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.ResultBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // OpenFolderButton
+            // 
+            this.OpenFolderButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.OpenFolderButton.Location = new System.Drawing.Point(13, 13);
+            this.OpenFolderButton.Name = "OpenFolderButton";
+            this.OpenFolderButton.Size = new System.Drawing.Size(528, 23);
+            this.OpenFolderButton.TabIndex = 0;
+            this.OpenFolderButton.Text = "Open Folder";
+            this.OpenFolderButton.UseVisualStyleBackColor = true;
+            this.OpenFolderButton.Click += new System.EventHandler(this.OpenFolderButton_Click);
+            // 
+            // ResultBox
+            // 
+            this.ResultBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.ResultBox.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ResultBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ResultBox.ForeColor = System.Drawing.SystemColors.Window;
+            this.ResultBox.Location = new System.Drawing.Point(13, 43);
+            this.ResultBox.Multiline = true;
+            this.ResultBox.Name = "ResultBox";
+            this.ResultBox.ReadOnly = true;
+            this.ResultBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.ResultBox.Size = new System.Drawing.Size(528, 384);
+            this.ResultBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(553, 439);
+            this.Controls.Add(this.ResultBox);
+            this.Controls.Add(this.OpenFolderButton);
+            this.Name = "Form1";
+            this.Text = "Folder Fix";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OpenFolderButton;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox ResultBox;
+    }
+}
+
---
+++ FolderFix/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FolderFix
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenFolderButton_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.RootFolder = Environment.SpecialFolder.Desktop;
+            folderBrowserDialog1.ShowNewFolderButton = false;
+            DialogResult r = folderBrowserDialog1.ShowDialog();
+
+            if (r == DialogResult.OK)
+            {
+                string selectedPath = folderBrowserDialog1.SelectedPath;
+
+                if(Directory.Exists(selectedPath))
+                {
+                    Log(selectedPath + " - Selected");
+
+                    //read folders in path
+                    string[] folders = Directory.GetDirectories(selectedPath);
+                    List<string> folderSets = new List<string>();
+                    string ext = "_files";
+
+                    foreach (string f in folders)
+                    {
+                        if (!f.EndsWith(ext))
+                        {
+                            string textFolder = f;
+                            string imgFolder = f + ext;
+
+                            if(Directory.Exists(f) && Directory.Exists(imgFolder))
+                            {
+                                if (!folderSets.Contains(textFolder))
+                                {
+                                    folderSets.Add(textFolder);
+                                    Log(textFolder + " found. " + folderSets.Count + " found so far.");
+
+                                    string[] textFiles = Directory.GetFiles(textFolder);
+                                    foreach (string filePath in textFiles)
+                                    {
+                                        string fileName = Path.GetFileName(filePath);
+                                        File.Move(textFolder + "\\" + fileName, imgFolder + "\\" + fileName);
+                                        Log("-- moved " + fileName);
+                                    }
+
+                                    Directory.Delete(textFolder);
+                                }
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    Log(selectedPath + " doesn't exist.");
+                }
+            }
+        }
+
+        private void Log(string m)
+        {
+            if (m.Length > 0)
+                ResultBox.Text += m + Environment.NewLine;
+        }
+    }
+}
